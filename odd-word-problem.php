ea9f045690905c4c110cf21127188a901844f30e
bjolar <bjorn.l.larsson@lycos.com>
2015-01-17 16:53:34 +0100

    first commit

---
+++ odd-word-problem.php
@@ -0,0 +1,32 @@
+<?php
+
+//Solution to the odd word problem
+//http://rosettacode.org/wiki/Odd_word_problem#PHP
+
+$odd = function ($prev) use ( &$odd ) {
+	$a = fgetc(STDIN);
+	if (!ctype_alpha($a)) {
+		$prev();
+		fwrite(STDOUT, $a);
+		return $a != '.';
+	}
+	$clos = function () use ($a , $prev) {
+		fwrite(STDOUT, $a);
+		$prev();
+	};
+	return $odd($clos);
+};
+$even = function () {
+	while (true) {
+		$c = fgetc(STDIN);
+		fwrite(STDOUT, $c);
+		if (!ctype_alpha($c)) {
+			return $c != ".";
+		}
+	}
+};
+$prev = function(){};
+$e = false;
+while ($e ? $odd($prev) : $even()) {
+	$e = !$e;
+}
